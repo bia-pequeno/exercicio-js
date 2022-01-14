baf50431e6f2390a9b2286aa3118e23d346054b9
bia-pequeno <bia.2017pequeno@gmail.com>
2022-01-14 10:07:17 -0300

    feito

---
+++ script.js
@@ -0,0 +1,17 @@
+let name="Beatriz"
+
+let number=5
+
+let year=2022
+
+let result= 2022 - number
+
+console.log(year - number)
+
+console.log("estou mexendo no cosole.log")
+
+let quartaFeira="false"  
+
+console.log(typeof quartaFeira)
+
+console.log()
